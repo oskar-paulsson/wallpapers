63a3f6cb4536be7e223097aa0189e4f228ce12c9
oskar <oskar@paulsson.dev>
2021-12-31 15:48:20 +0100

    added a script for moving pics into dir respective of their dimensions

---
+++ image_sorter.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/zsh
+
+echo Running image sorter script...
+
+# GEO=$(identify -verbose $1 | grep "geometry")
+
+# RES=$(echo $GEO | grep -o "2560x1440")
+
+# echo $RES
+
+# mkdir $RES
+
+# mv $1 $RES/$
+
+for FILE in *;
+do
+    echo $FILE
+    GEO=$(identify -verbose $FILE | grep "geometry")
+    RES=$(echo $GEO | grep -o -E "[0-9]{4}x[0-9]{3,4}")
+    if [[ ! -d $RES ]] && 
+    then
+       # mkdir $HOME/wallpapers/$RES
+       echo "Try to create directory $RES"
+       mkdir $RES
+    fi
+    mv $FILE $RES/ 
+done
+
+
